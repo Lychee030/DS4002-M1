d3dc5900e48bf5ccf509da2e7bc65d3af934ce2d
Samarth Saxena <60327706+ss9nfb@users.noreply.github.com>
2022-09-21 18:09:26 -0400

    Add files via upload

---
+++ figures/plots.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(ggplot2)
+setwd("C:/Users/Samarth Saxena/OneDrive - University of Virginia/College/Fall 2022/DS 4002/")
+raw_data <- read.csv("./salary_potential.csv")
+df = data.frame(raw_data)
+df <- na.omit(df)
+filtered_df <- subset(df, state_name == "Virginia")
+filtered_df
+max(filtered_df$stem_percent)
+match(max(filtered_df$stem_percent), filtered_df$stem_percent)
+# school with highest stem percentage - Virginia Military Institute
+filtered_df$name[match(max(filtered_df$stem_percent), filtered_df$stem_percent)]
+# school with minimum stem percentage - Jefferson College of Health Sciences
+filtered_df$name[match(min(filtered_df$stem_percent), filtered_df$stem_percent)]
+# difference in between early salaries - $10400
+filtered_df$early_career_pay[match(max(filtered_df$stem_percent), filtered_df$stem_percent)] - filtered_df$early_career_pay[match(min(filtered_df$stem_percent), filtered_df$stem_percent)]
+less_20df <- subset(filtered_df, stem_percent < 20)
+more_20df <- subset(filtered_df, stem_percent >= 20)
+less_20df
+more_20df
+average_less <- mean(less_20df$early_career_pay)
+average_more <- mean(more_20df$early_career_pay)
+# average early career salary of schools < 20 percent stem (51376.47)
+average_less
+# average early career salary of schools >= 20 percent stem (58387.5)
+average_more
+# difference of $7011.03
+average_more - average_less
